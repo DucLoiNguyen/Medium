d8940b5bc244c9f89353ca793657c9a1a6b6967b
LoiND <156526656+DucLoiNguyen@users.noreply.github.com>
2024-03-01 11:58:21 +0700

    commit

--- reactapp/src/pages/Notifi/notification.component.js
+++ reactapp/src/pages/Notifi/notification.component.js
@@ -12,23 +12,25 @@ function Notification() {
       <div className="flex justify-center">
         <div className="block w-full mx-6">
           <div className="mt-[52px] mb-7">
-            <h1 className="text-5xl text-bold">Notifications</h1>
-          </div>
-          <div className="pt-4 mt-10 bg-white">
-            <div className="flex place-items-center">
-              <div>
-                <button className="text-[#6b6b6b] hover:text-black" onClick={() => handleTabClick('1')}>
-                  <div className={`pb-4 mr-8 ${activeTab === "1" ? "text-black" : "hover:text-black"}`}>
-                    <p className="text-sm">All</p>
-                  </div>
-                </button>
-              </div>
-              <div>
-                <button className="text-[#6b6b6b] hover:text-black" onClick={() => handleTabClick('2')}>
-                  <div className={`pb-4 mr-8 ${activeTab === "2" ? "text-black" : "hover:text-black"}`}>
-                    <p className="text-sm">Response</p>
-                  </div>
-                </button>
+            <div className="mb-10">
+              <h1 className="text-5xl text-bold">Notifications</h1>
+            </div>
+            <div className="bg-white">
+              <div className="flex place-items-center">
+                <div>
+                  <button className="text-[#6b6b6b] hover:text-black" onClick={() => handleTabClick('1')}>
+                    <div className={`pb-4 mr-8 ${activeTab === "1" ? "text-black" : "hover:text-black"}`}>
+                      <p className="text-sm">All</p>
+                    </div>
+                  </button>
+                </div>
+                <div>
+                  <button className="text-[#6b6b6b] hover:text-black" onClick={() => handleTabClick('2')}>
+                    <div className={`pb-4 mr-8 ${activeTab === "2" ? "text-black" : "hover:text-black"}`}>
+                      <p className="text-sm">Response</p>
+                    </div>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
